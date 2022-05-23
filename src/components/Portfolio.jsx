d2faa3b816d6410f25ea1f3e4ec8a57293a44f6c
svskaushik <shaurya9702@gmail.com>
2022-05-23 21:21:40 +0530

    Portfolio Chart UI Improvement

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import { Line } from "react-chartjs-2";
 import { useMoralis } from "react-moralis";
+import { Spin } from "antd";
 import {
   Chart as ChartJS,
   LineElement,
@@ -37,6 +38,77 @@ ChartJS.register(
 //   });
 // }
 
+function getGradient(ctx, chartArea, data, scales) {
+  const { bottom } = chartArea;
+  const { y } = scales;
+  const gradientBorder = ctx.createLinearGradient(0, 0, 0, bottom);
+  const shift = y.getPixelForValue(data.datasets[0].data[0]) / bottom;
+  if (shift && shift > 0 && shift < 1) {
+    gradientBorder.addColorStop(0, "rgba(75, 192, 192, 1)");
+    gradientBorder.addColorStop(shift, "rgba(75, 192, 192, 1)");
+    gradientBorder.addColorStop(shift, "rgba(255, 26, 104, 1)");
+    gradientBorder.addColorStop(1, "rgba(255, 26, 104, 1)");
+  }
+  return gradientBorder;
+}
+
+function belowGradient(ctx, chartArea, data, scales) {
+  if (data.datasets[0].data[0] != null) {
+    const { bottom } = chartArea;
+    const { y } = scales;
+    const gradientBackground = ctx.createLinearGradient(
+      0,
+      y.getPixelForValue(data.datasets[0].data[0]),
+      0,
+      bottom,
+    );
+    gradientBackground.addColorStop(0, "rgba(255, 26, 104, 0.2)");
+    gradientBackground.addColorStop(1, "rgba(255, 26, 104, 0.8)");
+    return gradientBackground;
+  }
+}
+
+function aboveGradient(ctx, chartArea, data, scales) {
+  if (data.datasets[0].data[0] != null) {
+    const { top } = chartArea;
+    const { y } = scales;
+    const gradientBackground = ctx.createLinearGradient(
+      0,
+      y.getPixelForValue(data.datasets[0].data[0]),
+      0,
+      top,
+    );
+    gradientBackground.addColorStop(0, "rgba(75, 192, 192, 0.2)");
+    gradientBackground.addColorStop(1, "rgba(75, 192, 192, 0.8)");
+    return gradientBackground;
+  }
+}
+
+const dottedLine = {
+  id: "dottedLine",
+  beforeDatasetsDraw(chart) {
+    const {
+      ctx,
+      data,
+      chartArea: { left, right },
+      scales: { y },
+    } = chart;
+
+    if (data.datasets[0].data[0] != null) {
+      ctx.save();
+      ctx.beginPath();
+      ctx.lineWidth = 2.5;
+      ctx.setLineDash([1, 4]);
+      ctx.strokeStyle = "rgba(102, 102, 102, 0.6)";
+      ctx.moveTo(left, y.getPixelForValue(data.datasets[0].data[0]));
+      ctx.lineTo(right, y.getPixelForValue(data.datasets[0].data[0]));
+      ctx.stroke();
+      ctx.closePath();
+      ctx.setLineDash([]);
+    }
+  },
+};
+
 const addressSocket = {
   namespace: "address",
   socket: io(`${BASE_URL}address`, {
@@ -71,10 +143,12 @@ function Portfolio() {
   let [dataset, setDataset] = useState([]);
   let [timeStamps, setTimeStamps] = useState([]);
   let [values, setValues] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
   const { account } = useMoralis();
 
   useEffect(() => {
     async function fetcher() {
+      setIsLoading(true);
       const fetcher = await get(addressSocket, {
         scope: ["charts"],
         payload: {
@@ -86,9 +160,9 @@ function Portfolio() {
         },
       });
       const result = await fetcher;
-      console.log(result);
       //   console.log(result.payload.charts.others);
       await setDataset(result.payload.charts.others);
+      setIsLoading(false);
     }
     fetcher();
   }, [account]);
@@ -98,9 +172,12 @@ function Portfolio() {
 
   function separator(item) {
     _timeStamps.push(
-      new Date(item[0] * 1000).toLocaleTimeString([], {
+      new Date(item[0] * 1000).toLocaleString([], {
         hour: "2-digit",
         minute: "2-digit",
+        weekday: "long",
+        month: "long",
+        day: "numeric",
       }),
     );
     _values.push(item[1]);
@@ -112,6 +189,20 @@ function Portfolio() {
     setValues(_values);
   }, [dataset]);
 
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <Spin />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Line
@@ -119,37 +210,67 @@ function Portfolio() {
           labels: timeStamps,
           datasets: [
             {
-              label: "ETH Portfolio Value",
+              label: "Portfolio Value",
               data: values,
+              pointRadius: 0.1,
+              pointHitRadius: 100,
+              tension: 0.8,
+              borderWidth: 1,
               parsing: {
                 yAxisKey: "0",
               },
-              backgroundColor: ["rgba(0, 99, 232, 0.6)"],
-              showLine: "true",
+              borderColor: (context) => {
+                const chart = context.chart;
+                const { ctx, chartArea, data, scales } = chart;
+                if (!chartArea) {
+                  return null;
+                }
+                return getGradient(ctx, chartArea, data, scales);
+              },
               fill: {
-                target: "origin",
-                above: "rgba(0, 80, 250, 0.4)", // Area will be red above the origin
-                below: "rgb(0, 0, 255)", // And blue below the origin
+                target: {
+                  value: values[0],
+                },
+                above: (context) => {
+                  const chart = context.chart;
+                  const { ctx, chartArea, data, scales } = chart;
+                  if (!chartArea) {
+                    return null;
+                  }
+                  return aboveGradient(ctx, chartArea, data, scales);
+                },
+                below: (context) => {
+                  const chart = context.chart;
+                  const { ctx, chartArea, data, scales } = chart;
+                  if (!chartArea) {
+                    return null;
+                  }
+                  return belowGradient(ctx, chartArea, data, scales);
+                },
               },
             },
           ],
         }}
-        height={200}
-        width={200}
+        height={250}
         options={{
           maintainAspectRatio: false,
           responsive: true,
           plugins: {
             title: {
               display: true,
-              text: "ETH Portfolio Value (USD)",
+              text:
+                values != null
+                  ? `Portfolio Value $ ${values[values.length - 1]?.toFixed(2)}`
+                  : "(no values found)",
               color: "#fff",
             },
           },
           scales: {
             x: { display: false },
+            y: { display: false },
           },
         }}
+        plugins={[dottedLine]}
       />
     </div>
   );
